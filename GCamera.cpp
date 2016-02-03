4b44059f1d2942d67e8316c464008801d8b7702c
John Augustsson <soetfisk@live.se>
2016-02-03 11:38:48 +0100

    work work

--- GCamera.cpp
+++ GCamera.cpp
@@ -3,7 +3,7 @@
 //VEIW
 void GCamera::initViewMatrix()
 {
-
+	cView = XMMatrixLookAtLH(XMLoadFloat3(&cPosition), XMLoadFloat3(&cTarget), XMLoadFloat3(&cUp));
 }
 
 void GCamera::move(XMFLOAT3 direction)
@@ -25,15 +25,15 @@ void GCamera::setTarget(XMFLOAT3 nTarget)
 
 const XMFLOAT3 GCamera::getUp() //returns camera up vector
 {
-
+	return cUp;
 }
 const XMFLOAT3 GCamera::getLookAtTarget() //returns camera look at target vector
 {
-
+	return cTarget;
 }
 const XMMATRIX GCamera::getViewMatrix()
 {
-
+	return cView;
 }
 
 
@@ -53,12 +53,19 @@ void GCamera::setFarPlane(float farthest)
 
 const XMMATRIX GCamera::getProjMatrix()
 {
-
+	return cProj;
 }
 
-void GCamera::InitProjMatrix(const float angle, const float height, const float width)
+void GCamera::InitProjMatrix(const float angle, const float height, const float width,
+	const float nearest, const float farthest)
 {
+	frustAngle = angle;
+	cWidth = width;
+	cHeight = height;
+	frustFar = farthest;
+	frustNear = nearest;
 
+	cProj = XMMatrixPerspectiveFovLH(frustAngle, cHeight / cWidth, frustNear, frustFar);
 }
 
 //CON/DESTRUCTOR
@@ -66,6 +73,16 @@ GCamera::GCamera()
 {
 	cPosition = XMFLOAT3(0.0f, 0.0f, -1.0f);
 	cTarget = XMFLOAT3(0.0f, 0.0f, 0.0f);
+	cUp = XMFLOAT3(0.0f, 1.0f, 0.0f);
+
+	this->initViewMatrix();
+
+	frustAngle = 0.0f;
+	frustFar = 0.0f;
+	frustNear = 0.0f;
+
+	cHeight = 0.0f;
+	cWidth = 0.0f;
 
 
 }
