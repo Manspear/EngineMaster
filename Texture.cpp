d4fc6f498746cd5f56df0115b23e7de8ea28dbe9
Raxo94 <Oskar_swing@hotmail.com>
2016-02-04 15:33:58 +0100

    Included Window image Component
    These files will help with loading all kinds of textures

---
+++ Texture.cpp
@@ -0,0 +1,20 @@
+#include "Texture.h"
+#include "WICTextureLoader.h"
+
+void TextureClass::LoadTexture()
+{
+	//Windows imaging component
+	//DirectX Tool Kit. DirectX TK
+
+	
+	/*glGenTextures(1, &mTexture);
+	glBindTexture(GL_TEXTURE_2D, mTexture);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	HRESULT hr = WICTexImage2DFromFile(GL_TEXTURE_2D, 0, L"sample.ext");
+	if (FAILED(hr))
+	{
+		// Error
+	}*/
+}
