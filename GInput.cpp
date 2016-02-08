9a1d4e24f0ee3cf9b9e93e9e725d4cf0a4f14786
John Augustsson <soetfisk@live.se>
2016-02-08 17:05:43 +0100

    stuff

--- GInput.cpp
+++ GInput.cpp
@@ -38,10 +38,18 @@ void GInput::initialize(HINSTANCE hinstance, HWND hwnd, int screenWidth, int scr
 	mouseY = 0;
 
 	hr = DirectInput8Create(hinstance, DIRECTINPUT_VERSION, IID_IDirectInput8, (void**)&gDirectInput, NULL);
+
+	//init keyboard
 	hr = gDirectInput->CreateDevice(GUID_SysKeyboard, &gKeyboard, NULL);
 	hr = gKeyboard->SetDataFormat(&c_dfDIKeyboard);
 	hr = gKeyboard->SetCooperativeLevel(hwnd, DISCL_BACKGROUND | DISCL_EXCLUSIVE);
 	hr = gKeyboard->Acquire();
+
+	//init mouse
+	hr = gDirectInput->CreateDevice(GUID_SysMouse, &gMouse, NULL);
+	hr = gMouse->SetCooperativeLevel(hwnd, DISCL_BACKGROUND | DISCL_EXCLUSIVE);
+	hr = gMouse->SetDataFormat(&c_dfDIMouse);
+	hr = gMouse->Acquire();
 
 }
 void GInput::getKeyboardState()
@@ -51,13 +59,31 @@ void GInput::getKeyboardState()
 	
 	hr = gKeyboard->GetDeviceState((sizeof(unsigned char)<<8), (void*)keyState);
 	if (hr != S_OK)
-	{
 		hr = gKeyboard->Acquire();
-	}
+
 
 
 }
-void GInput::GetMouseLoc(int&, int&)
+void GInput::GetMouseLoc(int& x, int& y)
 {
+	HRESULT hr;
+	hr = gMouse->GetDeviceState(sizeof(DIMOUSESTATE), (void*)&mouseState);
+	if (hr != S_OK)
+		hr = gMouse->Acquire();
 
+	mouseX = mouseState.lX;
+	mouseY = mouseState.lY;
+
+	if (mouseX <= 0)
+		mouseX = 0;
+	if (mouseX > gScreenWidth)
+		mouseX = gScreenWidth;
+	if (mouseY <= 0)
+		mouseY = 0;
+	if (mouseY > gScreenHeight)
+		mouseY = gScreenHeight;
+
+	x = mouseX;
+	y = mouseY;
+	
 }
