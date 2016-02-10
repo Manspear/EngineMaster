2d1096c64f508c9bf58fcc7f8022b4cb56e58086
John Augustsson <soetfisk@live.se>
2016-02-10 12:27:51 +0100

    specular sturted

--- PixelShader.hlsl
+++ PixelShader.hlsl
@@ -43,7 +43,13 @@ float4 PS_main(PS_IN input) : SV_Target
 	input.normal = normalize(input.normal);
 	float lightIntensity;
 
+	//specular
+	float shinypower = 3.0f;
+	float4 specular = float4(1.0, 1.0, 1.0, 1.0);
+	float3 r = reflect(normalize(lightPos - input.worldPosition), input.normal);
+	float3 v = (input.worldPosition * -1.0);
 
+	//float3 sl = specular * pow(max(dot(r, v) 0.0), shinypower);
 
 
 	lightPos = normalize(lightPos);
