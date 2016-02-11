46345de4c31c70a2512fe4938018c5af6127d61c
Raxo94 <Oskar_swing@hotmail.com>
2016-02-11 11:47:37 +0100

    Made GModel skeleton

---
+++ GModel.h
@@ -0,0 +1,24 @@
+#ifndef GMODEL_H
+#define GMODEL_H
+#include "Engine.h"
+class GModel
+{
+private:
+public:
+	GModel();
+	~GModel();
+	FbxDawg modelLoader;
+	//vertex buffer
+};
+
+
+GModel::GModel()
+{
+}
+
+GModel::~GModel()
+{
+}
+
+#endif // !GMODEL_H
+
