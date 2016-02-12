578a410fea23803cb1a8176dd63e4d4428d3f9a6
Manspear <albincarlsson13@gmail.com>
2016-02-12 15:22:38 +0100

    Now MasterEngine supports several meshes with different textures

--- GModel.cpp
+++ GModel.cpp
@@ -1,12 +1,16 @@
 #include "GModel.h"
+#include "WICTextureLoader.h"
+
 GModel::GModel()
 {
-
+	//	//this->objectWorldMatrix = XMMatrixIdentity();
 }
 
 GModel::~GModel()
 {
-
+	//modelVertexBuffer->Release(); //This gets released...Somewhere.
+	//modelTextureView[0]->Release();
+	//modelTextureView[1]->Release();
 }
 //void GModel::setPosition(XMMATRIX translationMatrix) 
 //{
@@ -19,7 +23,8 @@ void GModel::load(const char* fbxFilePath, ID3D11Device* gDevice) //This is used
 	FbxDawg modelLoader;
 	modelLoader.loadModels(fbxFilePath);
 	//Note: Doing this vvvvvv may cause problems according to Martin, since it's vector = vector
-	modelVertices = modelLoader.modelVertexList; 
+	this->modelVertices = modelLoader.modelVertexList; 
+	this->modelTextureFilepath = modelLoader.textureFilepath;
 
 	D3D11_BUFFER_DESC bufferDesc;
 	memset(&bufferDesc, 0, sizeof(bufferDesc));
@@ -31,6 +36,16 @@ void GModel::load(const char* fbxFilePath, ID3D11Device* gDevice) //This is used
 	data.pSysMem = modelVertices.data();
 
 	gDevice->CreateBuffer(&bufferDesc, &data, &modelVertexBuffer);
+	
+	#pragma region //Import from File
+	HRESULT hr;
+	ID3D11ShaderResourceView * Texture;
+	CoInitialize(NULL);
+	//Need to have this be part of the Render-looping-through-objects-loop. That is: not having modelList[0]
+	hr = DirectX::CreateWICTextureFromFile(gDevice, modelTextureFilepath.c_str(), NULL, &modelTextureView[0]);//wstring äger functionen c_str som är en getFucntion till wchar_t* som finns redan
+	hr = DirectX::CreateWICTextureFromFile(gDevice, L"./Images/normal_map.jpg", NULL, &modelTextureView[1]);
+	//(d3d11DeviceInterface, d3d11DeviceContextInterface, L"test.bmp", 0, D3D11_USAGE_STAGING, 0, D3D11_CPU_ACCESS_READ, 0, 0, &pTex2D, NULL);
+	#pragma endregion 
 }; 
 
 
--- GModel.h
+++ GModel.h
@@ -13,25 +13,21 @@ public:
 	GModel();
 	~GModel();
 	ID3D11Buffer* modelVertexBuffer = nullptr;
+	ID3D11ShaderResourceView* modelTextureView[2]; //texture and normal map
 	//vertex buffer, constant buffer, 
 	//share projection and view, but have different world-view.
 	//void setPosition(DirectX::XMMATRIX translationMatrix);
 	void renderModel();
 	std::vector<MyVertexStruct> modelVertices; //This holds the vertices.
+	std::wstring modelTextureFilepath; //THis holds the texture's file-path
 	//struct with vertex positions held by FbxDawg
 	void load(const char* fbxFilePath, ID3D11Device* gDevice); //<-- Loads the model. Means that modelLoader is called.
 	//DirectX::XMMATRIX objectWorldMatrix;
 };
 
-//GModel::GModel()
-//{
-//	//this->objectWorldMatrix = XMMatrixIdentity();
-//}
 
-//GModel::~GModel()
-//{
-//	modelVertexBuffer->Release(); //When GModel is removed, it's buffer is released
-//}
+
+
 
 
 #endif // !GMODEL_H
