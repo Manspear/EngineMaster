6f9782dff951d2dabeda6452c7c3a648b3795bd3
Manspear <albincarlsson13@gmail.com>
2016-02-16 15:43:09 +0100

    Fixed weird shit in pixelshader

--- PixelShader.hlsl
+++ PixelShader.hlsl
@@ -30,12 +30,9 @@ struct PS_IN
 float4 PS_main(PS_IN input) : SV_Target
 {
 	//Light Inits
-<<<<<<< HEAD
+
 	//Why is the lightposition a vector pointing at the light position? It's weird. 
-	float3 lightPos = normalize(float3 (4, 3, -3) - input.worldPosition); //Vector from worldPosition to camera(is correct)
-=======
 	float3 lightPos = normalize(float3 (4, 3, -3) - input.pixelPosition); //Vector from pixelPosition to camera(is correct)
->>>>>>> refs/remotes/origin/Albin's-Lair-2
 	float4 ambientLightColor = { 0.2, 0.2, 0.2, 0 };
 	float4 diffuseColor = float4(1,1,1,1);
 
