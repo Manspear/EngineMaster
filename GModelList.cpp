78c83623363a6da95f3e5a23e7dcae2d8b494e3b
Raxo94 <Oskar_swing@hotmail.com>
2016-02-18 15:29:22 +0100

    comment out detailed box
    for faster loading. Work in progress

--- GModelList.cpp
+++ GModelList.cpp
@@ -12,18 +12,18 @@ GModelList::~GModelList()
 
 void GModelList::initializeModels(ID3D11Device* gDevice, ID3D11DeviceContext * gDeviceContext) {
 	//Here create the dynamic GModel-Array:
-	this->numberOfModels = 3;
+	this->numberOfModels = 2;
 	this->modelList = new GModel[this->numberOfModels];
 
 	this->modelList[0].load(".\\Objects/ItsBoxxyTextured.fbx", gDevice, gDeviceContext); 
 	this->modelList[1].load(".\\Objects/itsBoxxyTextured.fbx", gDevice, gDeviceContext);
-	this->modelList[2].load(".\\Objects/ItsSofty.fbx", gDevice, gDeviceContext);
+	//this->modelList[2].load(".\\Objects/ItsSofty.fbx", gDevice, gDeviceContext);
 	
 	//this->modelList[3].load(".\\itsBoxxy", gDevice);
 
 	modelList[0].setPosition(XMFLOAT4(2, 0, 0, 1), gDeviceContext);
 	modelList[1].setPosition(XMFLOAT4(0, 0, 2, 1), gDeviceContext);
-	modelList[2].setPosition(XMFLOAT4(-2, 0, 2, 1), gDeviceContext);
+	//modelList[2].setPosition(XMFLOAT4(-2, 0, 2, 1), gDeviceContext);
 }
 
 GModel* GModelList::getModelList() {
