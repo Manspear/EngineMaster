cd82766c81727ac9333a2abedea1d5b18465159a
John Augustsson <soetfisk@live.se>
2016-03-30 14:39:33 +0200

    defererer H

--- DeferredBuffer.h
+++ DeferredBuffer.h
@@ -1,13 +1,36 @@
 #pragma
 
+#ifndef DEFERREDBUFFER_H
+
 #include <d3d11.h>
-#include <d3dx10math.h>
+#include <directXMath.h>
 
-class DefferedBuffers
+const int BUFFER_COUNT = 2;
+
+class DeferredBuffer
 {
+public:
+	DeferredBuffer();
+	DeferredBuffer(const DeferredBuffer&);
+	~DeferredBuffer();
 
+	void initialize(ID3D11Device *, int, int, float, float);
+	void Shutdown();
 
+	void setRenderTarget(ID3D11DeviceContext*);
+	void clearRenderTargets(ID3D11DeviceContext*, float, float, float, float);
 
+	ID3D11ShaderResourceView getShaderResource(int);
 
+private:
+	int textureWidth, textureHeight;
 
+	ID3D11Texture2D* renderTargerTextureArray[BUFFER_COUNT];
+	ID3D11RenderTargetView* renderTargetVíewArray[BUFFER_COUNT];
+	ID3D11ShaderResourceView* shaderResourceViewArray[BUFFER_COUNT];
+	ID3D11Texture2D* depthStencilBuffer;
+	ID3D11DepthStencilView* depthStencilView;
+	D3D11_VIEWPORT viewport;
 };
+
+#endif DEFERREDBUFFER_H
