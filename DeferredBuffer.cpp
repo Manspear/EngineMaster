29c77273562fd5c4ca2661e0482dadf982345742
soetfisk <augustssonjohn@hotmail.com>
2016-03-31 15:56:57 +0200

    deffering moaer deferrerererr

--- DeferredBuffer.cpp
+++ DeferredBuffer.cpp
@@ -21,7 +21,7 @@ DeferredBuffer::~DeferredBuffer()
 {
 }
 
-void DeferredBuffer::initialize(ID3D11Device * gDevice, int textureWidth, int textureHeight, float screenDepth, float sceenNear)
+void DeferredBuffer::initialize(ID3D11Device * gDevice, ID3D11DeviceContext * gDeviceContext, int textureWidth, int textureHeight, float screenDepth, float sceenNear)
 {
 	D3D11_TEXTURE2D_DESC textureDesc;
 	HRESULT rs;
@@ -30,6 +30,8 @@ void DeferredBuffer::initialize(ID3D11Device * gDevice, int textureWidth, int te
 	D3D11_TEXTURE2D_DESC depthBufferDesc;
 	D3D11_DEPTH_STENCIL_VIEW_DESC depthStencilVeiwDesc;
 	
+	this->gDevice = gDevice;
+	this->gDeviceContext = gDeviceContext;
 	this->textureHeight = textureHeight;
 	this->textureWidth = textureWidth;
 
@@ -49,25 +51,96 @@ void DeferredBuffer::initialize(ID3D11Device * gDevice, int textureWidth, int te
 
 	for (int i = 0; i < BUFFER_COUNT; i++)
 	{
-		rs = gDevice->CreateTexture2D(&textureDesc)
-
+		rs = gDevice->CreateTexture2D(&textureDesc, NULL, &renderTargerTextureArray[i]);
 	}
 
+	shaderResourceViewDesc.Format = textureDesc.Format;
+	shaderResourceViewDesc.ViewDimension = D3D11_SRV_DIMENSION_TEXTURE2D;
+	shaderResourceViewDesc.Texture2D.MostDetailedMip = 0;
+	shaderResourceViewDesc.Texture2D.MipLevels = 1;
+
+	for (int i = 1; i < BUFFER_COUNT; i++)
+	{
+		rs = gDevice->CreateShaderResourceView(renderTargerTextureArray[i], &shaderResourceViewDesc, &shaderResourceViewArray[i]);
+	}
+
+	depthBufferDesc.Width = textureWidth;
+	depthBufferDesc.Height = textureHeight;
+	depthBufferDesc.MipLevels = 1;
+	depthBufferDesc.ArraySize = 1;
+	depthBufferDesc.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
+	depthBufferDesc.SampleDesc.Count = 1;
+	depthBufferDesc.SampleDesc.Quality = 0;
+	depthBufferDesc.Usage = D3D11_USAGE_DEFAULT;
+	depthBufferDesc.BindFlags = D3D11_BIND_DEPTH_STENCIL;
+	depthBufferDesc.CPUAccessFlags = 0;
+	depthBufferDesc.MiscFlags = 0;
+
+	rs = gDevice->CreateTexture2D(&depthBufferDesc, NULL, &depthStencilBuffer);
+
+	ZeroMemory(&depthStencilVeiwDesc, sizeof(depthStencilVeiwDesc));
+
+	depthStencilVeiwDesc.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
+	depthStencilVeiwDesc.ViewDimension = D3D11_DSV_DIMENSION_TEXTURE2D;
+	depthStencilVeiwDesc.Texture2D.MipSlice = 0;
+
+	rs = gDevice->CreateDepthStencilView(depthStencilBuffer, &depthStencilVeiwDesc, &depthStencilView);
+
+	viewport.Width = (float)textureWidth;
+	viewport.Height = (float)textureHeight;
+	viewport.MinDepth = 0.0f;
+	viewport.MaxDepth = 1.0f;
+	viewport.TopLeftX = 0.0f;
+	viewport.TopLeftY = 0.0f;
+
+	return;
 }
 
 void DeferredBuffer::Shutdown()
 {
+	if (depthStencilView)
+		depthStencilView->Release();
+	if (depthStencilBuffer)
+		depthStencilBuffer->Release();
+
+	for (int i = 0; i < BUFFER_COUNT; i++)
+	{
+		if (shaderResourceViewArray[i])
+			shaderResourceViewArray[i]->Release();
+		if (renderTargerTextureArray[i])
+			renderTargerTextureArray[i]->Release();
+		if (renderTargetVíewArray[i])
+			renderTargetVíewArray[i]->Release();
+	}
+	return;
 }
 
-void DeferredBuffer::setRenderTarget(ID3D11DeviceContext *)
+void DeferredBuffer::setRenderTarget()
 {
+	gDeviceContext->OMSetRenderTargets(BUFFER_COUNT, renderTargetVíewArray, depthStencilView);
+	gDeviceContext->RSSetViewports(1, &viewport);
 }
 
-void DeferredBuffer::clearRenderTargets(ID3D11DeviceContext *, float, float, float, float)
+void DeferredBuffer::clearRenderTargets(float red, float green, float blue, float alpha)
 {
+	float color[4];
+
+	color[0] = red;
+	color[1] = green;
+	color[2] = blue; 
+	color[3] = alpha;
+
+	for (int i = 0; i < BUFFER_COUNT; i++)
+	{
+		gDeviceContext->ClearRenderTargetView(renderTargetVíewArray[i], color);
+	}
+
+	gDeviceContext->ClearDepthStencilView(depthStencilView, D3D11_CLEAR_DEPTH, 1.0f, 0);
+
+	return;
 }
 
-ID3D11ShaderResourceView DeferredBuffer::getShaderResource(int)
+ID3D11ShaderResourceView * DeferredBuffer::getShaderResource(int view)
 {
-	return ID3D11ShaderResourceView();
+	return shaderResourceViewArray[view];
 }
--- DeferredBuffer.h
+++ DeferredBuffer.h
@@ -14,16 +14,19 @@ public:
 	DeferredBuffer(const DeferredBuffer&);
 	~DeferredBuffer();
 
-	void initialize(ID3D11Device *, int, int, float, float);
+	void initialize(ID3D11Device *, ID3D11DeviceContext *, int, int, float, float);
 	void Shutdown();
 
-	void setRenderTarget(ID3D11DeviceContext*);
-	void clearRenderTargets(ID3D11DeviceContext*, float, float, float, float);
+	void setRenderTarget();
+	void clearRenderTargets(float, float, float, float);
 
-	ID3D11ShaderResourceView getShaderResource(int);
+	ID3D11ShaderResourceView * getShaderResource(int);
 
 private:
 	int textureWidth, textureHeight;
+
+	ID3D11Device * gDevice;
+	ID3D11DeviceContext * gDeviceContext;
 
 	ID3D11Texture2D* renderTargerTextureArray[BUFFER_COUNT];
 	ID3D11RenderTargetView* renderTargetVíewArray[BUFFER_COUNT];
